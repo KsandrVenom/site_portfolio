532c9ee9cfa790f612dd54e686e8559356066d3a
KsandrVenom <clif007@yandex.ru>
2020-04-19 21:27:32 +0300

    заменил button на chekbox, дописал код на JS !=)) , добавил селекторы chekbox в оба файла стилей

--- switch_themes.js
+++ switch_themes.js
@@ -1,7 +1,14 @@
 "use strict";
 
-let themeButton = document.querySelector('.switch-theme');
+let themeButton = document.querySelector('input');
+let themeStyle = document.querySelector('link');
 
-themeButton.onclick = function {
-    // до лучших времён
-};
+
+themeButton.onclick = function () {
+    if (themeButton.checked) {
+        themeStyle.href = "pro-dark-theme-style.css";
+    } else { 
+        themeStyle.href = "style.css";
+    };
+}; 
+
